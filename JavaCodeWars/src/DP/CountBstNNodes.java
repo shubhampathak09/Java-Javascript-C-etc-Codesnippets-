6aa1179eeeb18d4565176a87e39901619ff40238
Shubham <95shubham1@gmail.com>
2021-08-30 22:35:43 +0530

    codes

---
+++ JavaCodeWars/src/DP/CountBstNNodes.java
@@ -0,0 +1,28 @@
+package DP;
+
+public class CountBstNNodes {
+
+    public static void main(String[] args)
+    {
+
+
+        int n=5;
+
+        int[] dp=new int[n+1];
+
+        dp[0]=1;
+        dp[1]=1;
+
+        for(int  i=2;i<=n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                dp[i]=dp[j]*dp[i-j-1];
+            }
+        }
+
+        System.out.println("Total bst possible with n keys.."+dp[n]);
+
+    }
+
+}
