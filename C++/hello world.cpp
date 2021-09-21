a3d867732c690335443f6aea2c695ab002951551
Shubham <95shubham1@gmail.com>
2021-09-21 19:00:19 +0530

    hello

---
+++ C++/hello world.cpp
@@ -0,0 +1,25 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+int x;
+
+while(x)
+putchar(t>>(((x>>=3)&7)<<3)&0xFF);
+}
+
+
+Shailendra Patel12:34 PM
+af_c_id={Campaign_id}
+af_ad_id={Banner_id}
+af_sub1={Ref_ID}
+af_sub2={ad_platform}
+pid=paytm_int
+clickid={click_id}
+advertising_id={gaid}
+af_click_lookback=7d  this should be at least 7d.
+Shailendra Patel12:39 PM
+https://support.appsflyer.com/hc/en-us/articles/360017189177#enable-retargeting
