4152cba5e6c809eea69fcac833a161a861befc05
Shubham <95shubham1@gmail.com>
2021-09-23 10:29:03 +0530

    some fixes

--- C++/hello world.cpp
+++ C++/hello world.cpp
@@ -5,21 +5,9 @@ using namespace std;
 int main()
 {
 
-int x;
-
-while(x)
-putchar(t>>(((x>>=3)&7)<<3)&0xFF);
+ uint64_t dx=0x357620655410;
+ 
+ while(dx) putchar(0x726F6C6564574820 >> (((dx>>=4)&0377)<<3));
+ 
 }
 
-
-Shailendra Patel12:34 PM
-af_c_id={Campaign_id}
-af_ad_id={Banner_id}
-af_sub1={Ref_ID}
-af_sub2={ad_platform}
-pid=paytm_int
-clickid={click_id}
-advertising_id={gaid}
-af_click_lookback=7d  this should be at least 7d.
-Shailendra Patel12:39 PM
-https://support.appsflyer.com/hc/en-us/articles/360017189177#enable-retargeting
