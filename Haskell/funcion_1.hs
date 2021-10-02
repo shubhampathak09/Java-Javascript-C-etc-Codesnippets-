93cc3aa398186daa6309ae8bfc4da0e4492006ac
Shubham <95shubham1@gmail.com>
2021-10-02 22:33:16 +0530

    Intro to  Haskell functions

--- Haskell/funcion_1.hs
+++ Haskell/funcion_1.hs
@@ -7,3 +7,8 @@ doubleMe x = x + x
 --now redifing doubleUS Using doubleMe
 
 doubleUs x y = doubleMe x + doubleMe y
+
+
+doubleSmallNumber x = if x > 100
+                      then x 
+					  else x*2
