de8333c4ac6e8aba9e76534e761ad83e5e61bb32
Shubham <95shubham1@gmail.com>
2021-10-03 21:29:07 +0530

    list comprehensions

--- Haskell/lists.hs
+++ Haskell/lists.hs
@@ -4,9 +4,12 @@ h=head lostNummbers
 
 t=tail lostNummbers
 
+xnw=replicate 2 lostNummbers
 
 --HEAD AND TAIL
 
 -- one common task is append lists togethere its done using  ++ 
 
- 
+ex1=take 10 [2,4..]
+
+pr1=[x*2 | x<- [1..10], x*2>=12]
