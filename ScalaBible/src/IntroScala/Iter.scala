846c395e82a6397fc68a0833ec8f2ed49a72c8ae
Shubham <95shubham1@gmail.com>
2021-10-08 17:56:52 +0530

    codes on lambda scala

--- ScalaBible/src/IntroScala/Iter.scala
+++ ScalaBible/src/IntroScala/Iter.scala
@@ -11,4 +11,18 @@ object Iter extends App{
 
   // more examples
 
+  val square= (x:Double)=> x*x
+
+  val addThreeNum = (x:Int,y:Int,z:Int) => x+y+z
+ // def square(x:Double):Double=x*x
+
+  println(square(23))
+
+  println(addThreeNum(3,4,5))
+
+ // val sq2: Double => Double = x => x*x
+
+  val twice: Double =>Double =_*2
+
+  println(twice(4))
 }
