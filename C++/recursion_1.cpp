9a3507f19257a5a40f5f28021d71b8588170d386
Shubham <95shubham1@gmail.com>
2021-10-09 23:34:44 +0530

    codes for recusion

---
+++ C++/recursion_1.cpp
@@ -0,0 +1,47 @@
+// recur 1
+
+// explain the functionality of following function
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int fun1(int x,int y)
+{
+	
+	
+	if(x==0)
+	{
+		return y;
+	}
+	
+	return fun1(x-1,x+y);
+	
+}
+
+
+int main()
+{
+
+int x=5;
+int y=2;
+
+// 5,2   4 , 5+2 -> 3,4+5+2, 2,3+4+5+2, 1,2+3+4+5+2, 0,1+2+3+4+5+2  =17   
+
+
+int ans=fun1(x,y);
+
+
+cout<<ans<<endl;
+
+
+return 0;
+
+//
+//
+	
+	
+	
+}
