3194b8e3cca331f16f99acea4415b1d254e11568
Shubham <95shubham1@gmail.com>
2021-10-19 19:55:41 +0530

    gt

---
+++ JavaCodeWars/src/Trees/TreeStructure.java
@@ -0,0 +1,21 @@
+package Trees;
+
+import java.util.ArrayList;
+
+public class TreeStructure {
+
+
+    private class Node
+    {
+        int val;
+        ArrayList<Node> children=new ArrayList<>();
+    }
+
+    public static void main(String[] args){
+
+        
+
+
+    }
+
+}
