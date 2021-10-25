ad1cbac6cd0251ccc8ddb16190eba2476df1b068
Shubham <95shubham1@gmail.com>
2021-10-25 09:59:02 +0530

    pattern 5

--- C++/pattern4.cpp
+++ C++/pattern4.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main()
 {
 	
-	
+	cout<<"Pattern 4";
+	cout<<endl;
 	for(int i=0;i<5;i++)
 	{
 		for(int j=0;j<5;j++)
@@ -14,5 +15,20 @@ int main()
 		}
 		cout<<endl;
 	}
+	cout<<endl;
+	
+	cout<<"Pattern 5";
+	cout<<endl;
+	for(int i=0;i<5;i++)
+	{
+		for(int j=0;j<5;j++)
+		{
+			cout<<(5-j)<<" ";
+			
+		}
+		cout<<endl;
+	}
+	
+	cout<<endl;
 	
 }
