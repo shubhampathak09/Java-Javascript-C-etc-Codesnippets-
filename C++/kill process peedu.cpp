9907eae064a3cdbc113035f6a3b75b623f57103a
Shubham <95shubham1@gmail.com>
2021-11-15 01:51:30 +0530

    pseduo codes for kill process

---
+++ C++/kill process peedu.cpp
@@ -0,0 +1,57 @@
+// kill process
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class node{
+	
+	
+	int data;
+	vector<node>children;
+	
+	
+	node(int data){
+		
+		this->data=data;
+		
+		
+	}
+	
+	parent=new node() //  assume
+
+	
+	
+	List<Integer>killProcess(List<Integer>pid,List<Integer>ppid,int kill){
+		
+		
+       for(int i=0;i<pid.length;i++){
+       	
+       	if(ppid[i]!=0){
+       		
+       		node sample =new nde
+			   sample.childen[ppid[i]].add(pid[i])
+			          		
+       		
+		   }else
+		   {
+		       parent.children[ppid]   	
+		   }
+	   }		
+	   
+	   // tree done
+		
+		reomve the stuc
+		
+		
+		
+	}
+	
+	
+};
+
+
+int main()
+{
+	
+	
+}
