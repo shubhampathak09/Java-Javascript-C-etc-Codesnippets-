016e319b5517c72afcc13dd669a309bcab6de1bb
Shubham <95shubham1@gmail.com>
2021-11-27 23:11:54 +0530

    uva 10911 matching quiz

---
+++ codes november 2021/check adjacent words similar.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+bool isadj(string a,string b){
+	
+	
+	int ct=0;
+	
+	for(int i=0;i<a.length();i++){
+		
+		if(a[i]!=b[i])
+		ct++;
+		
+	}
+	
+	if(ct==1)
+	{
+		return true;
+	}else
+	{
+		return false;
+	}
+	
+}
+
+int main()
+{
+	
+	cout<<isadj("SPOON","SPOOO")<<endl;
+	
+	cout<<isadj("SPOON","QUIET")<<endl;
+	
+}
