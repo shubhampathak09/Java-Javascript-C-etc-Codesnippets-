f01b6e0a54b24611e28dd8f2c84cc5002f476e19
Shubham <95shubham1@gmail.com>
2022-01-09 22:17:50 +0530

    Generic tree -1

---
+++ JavaCodeWars/src/Trees/GenericTree.java
@@ -0,0 +1,23 @@
+package Trees;
+
+import java.util.ArrayList;
+
+public class GenericTree {
+
+private class Node{
+
+    int data;
+    ArrayList<Node>children=new ArrayList<>();
+
+
+}
+
+public static void main(String[] args){
+
+
+    Node root;
+    
+
+}
+
+}
