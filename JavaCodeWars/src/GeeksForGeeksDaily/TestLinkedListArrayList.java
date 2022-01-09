af8d38d05b7ba955cc397be037dc8b3c8284b76b
Shubham <95shubham1@gmail.com>
2022-01-09 12:53:48 +0530

    tutorial linked list and array list

---
+++ JavaCodeWars/src/GeeksForGeeksDaily/TestLinkedListArrayList.java
@@ -0,0 +1,52 @@
+package GeeksForGeeksDaily;
+
+import LeetCodeDaily.LinkedListBinaryToInteger;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class TestLinkedListArrayList {
+
+
+    public static void main(String[] args){
+
+        LinkedList<String> names=new LinkedList<>();
+
+        names.add("Shubham");
+        names.add("April");
+        names.add("June");
+        names.add("May");
+
+
+        ArrayList<String>namesArrayList=new ArrayList<>();
+
+        namesArrayList.add("Jerry");
+        namesArrayList.add("cat");
+        namesArrayList.add("feed");
+        namesArrayList.add("shubham");
+
+        // Some Significant things in LinkedList and Array
+
+
+        // insertions and deletions are way faster in Linked List compared to Aray List
+
+        // get operations are faster ina array List
+
+
+        // Depends on problems
+
+        //small lists both will  be faster
+
+        // for Larges lists it plas significant things
+
+        // retiteive some values Array List is best bet
+
+        // but if we are asdding something or deleeting somethink linked list are best
+
+
+
+
+
+    }
+
+}
--- codes november 2021/Destination city java code.cpp
+++ codes november 2021/Destination city java code.cpp
@@ -34,4 +34,53 @@ class Solution {
 }
 
 
+
+class Solution {
+    public String destCity(List<List<String>> paths) {
+     
+        String ans="";
+        
+        HashMap<String,ArrayList<String>>hm=new HashMap<>();
+        
+        
+        HashSet<String>city=new HashSet<>();
+        
+        for(int i=0;i<paths.size();i++){
+            
+            String sourceCity=paths.get(i).get(0);
+            String destCity=paths.get(i).get(1);
+            
+            city.add(sourceCity);
+            city.add(destCity);
+            
+            ArrayList<String>children;
+            
+            
+            if(hm.containsKey(sourceCity)){
+                children=hm.get(sourceCity);
+            }else
+            {
+                children=new ArrayList<>();
+                hm.put(sourceCity,children);
+            }
+            children.add(destCity);
+        }
+        
+        
+        for(String s:city){
+            
+        //    System.out.println(s);
+            
+            if(!hm.containsKey(s)){
+                ans= s;
+            }
+        }
+        
+        return ans;
+    }
+}
+
+// second modiied solution ~~ not great but gets accepted
+
+
 // do not run if not noob
