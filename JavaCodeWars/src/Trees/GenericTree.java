69f77d78a0fa192b02ff7d78b46a8443254cf588
Shubham <95shubham1@gmail.com>
2022-01-09 22:26:57 +0530

    generic tree constructor

--- JavaCodeWars/src/Trees/GenericTree.java
+++ JavaCodeWars/src/Trees/GenericTree.java
@@ -1,10 +1,11 @@
 package Trees;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class GenericTree {
 
-private class Node{
+private static class Node{
 
     int data;
     ArrayList<Node>children=new ArrayList<>();
@@ -12,11 +13,44 @@ private class Node{
 
 }
 
+
+
+
 public static void main(String[] args){
 
 
     Node root;
-    
+
+    int[] arr={10,20,50,-1,60,-1,-1,30,70,-1,80,110,-1,120,-1,-1,90,-1,-1,40,100,-1,-1,-1,};
+
+
+    Stack<Node>st=new Stack<>() ;
+    for(int i=0;i<arr.length;i++){
+
+        if(arr[i]==-1){
+            st.pop();
+        }else
+        {
+
+            Node t=new Node();
+            t.data=arr[i];
+
+            if(st.size()>0) {
+                st.peek().children.add(t);
+
+            }
+            else
+            {
+                root=t;
+
+            }
+              st.push(t);
+
+        }
+
+
+
+    }
 
 }
 
