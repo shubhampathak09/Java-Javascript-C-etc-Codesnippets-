9947d5e2df19d58383ee7663d8dc5d1f1543db0c
Shubham <95shubham1@gmail.com>
2022-01-15 23:12:23 +0530

    single digit pr

--- codes november 2021/single digit prime l r.cpp
+++ codes november 2021/single digit prime l r.cpp
@@ -8,14 +8,42 @@ vector<int>prime(n);
 
 void sieve(){
 	
-	vector<int>isprime(n);
 	
+	prime[0]=prime[1]=0;
+	for(int i=2;i*i<=n;i++){
+		
+		if(prime[i]==1){
+			
+			for(int k=i*i;k<=n;k++){
+				prime[k]=0;
+			}
+			
+		}
+		
+	}
+}
+
+bool isSingleDigitPrime(int n){
+	
+	int k=n%9 ==0 ? 9:n%9;
+	
+	return prime[k]==1;
 	
 }
 
 int main()
 {
 	
+	int l=4;
+	int r=10;
 	
+	int ct=0;
 	
+	for(int i=l;i<=r;i++){
+		
+		if(prime[i]&&isSingleDigtPrime(i))
+		ct++;
+	}
+	
+	cout<<ct;
 }
