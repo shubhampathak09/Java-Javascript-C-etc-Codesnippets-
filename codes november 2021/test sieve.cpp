67d6a8f6a3466fd31f65481407176fc1d7d4ff19
Shubham <95shubham1@gmail.com>
2022-01-15 14:15:17 +0530

    sieve some changes

--- codes november 2021/test sieve.cpp
+++ codes november 2021/test sieve.cpp
@@ -12,12 +12,12 @@ for(int i=0;i<10000;i++)
 	num[0]=num[1]=0;
 	
 	
-	for(int i=2;i*i<1000;i++){
+	for(int i=2;i*i<10000;i++){
 		
 		if(num[i]==1){
 			
 			
-			for(int k=i*i;k<1000;k+=i){
+			for(int k=i*i;k<10000;k+=i){
 				
 			num[k]=0;	
 				
@@ -38,8 +38,8 @@ int main(){
 	
 	cout<<num[3]<<" "<<num[4]<<" ";
     
-    
-    for(int i=500;i<1000;i++){
+    cout<<"printing primes from 500 to 10000.."<<endl;
+    for(int i=500;i<10000;i++){
     	if(num[i]==1){
     		
     		cout<<i<<endl;
