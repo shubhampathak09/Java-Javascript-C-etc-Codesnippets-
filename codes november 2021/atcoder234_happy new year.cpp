33794d34df39c51f6ae1c403f219c042d605c1d4
Shubham <95shubham1@gmail.com>
2022-01-16 22:47:53 +0530

    happy new yeear

--- codes november 2021/atcoder234_happy new year.cpp
+++ codes november 2021/atcoder234_happy new year.cpp
@@ -1,3 +1,16 @@
+// Happy new Year 2022
+
+/*
+Problem Statement
+Among the positive integers that consist of 0's and 2's when written in base 10, find the K-th smallest integer.
+
+Constraints
+K is an integer between 1 and 10 
+18
+  (inclusive).
+  */
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
