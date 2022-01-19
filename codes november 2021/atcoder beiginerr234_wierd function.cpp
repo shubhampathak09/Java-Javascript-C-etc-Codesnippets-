73e9c3d76fb489c9c68e824779e41cabce963ba4
Shubham <95shubham1@gmail.com>
2022-01-20 00:21:26 +0530

    cdc

--- codes november 2021/atcoder beiginerr234_wierd function.cpp
+++ codes november 2021/atcoder beiginerr234_wierd function.cpp
@@ -2,6 +2,24 @@
 using namespace std;
 
 
+
+//https://stackoverflow.com/questions/30988002/what-does-rebalancing-mean-in-apache-kafka-context
+
+
+//https://stackoverflow.com/questions/56647442/kafka-keeps-rebalancing-consumers
+
+//https://stackoverflow.com/questions/30988002/what-does-rebalancing-mean-in-apache-kafka-context
+
+//https://stackoverflow.com/questions/42706463/what-factors-determine-consumer-group-rebalancing-time
+
+//https://stackoverflow.com/questions/30988002/what-does-rebalancing-mean-in-apache-kafka-context
+
+
+//https://stackoverflow.com/questions/39730126/difference-between-session-timeout-ms-and-max-poll-interval-ms-for-kafka-0-10
+
+
+//
+
 int f(int x){
 	
 	return x*x+2*x+3;
