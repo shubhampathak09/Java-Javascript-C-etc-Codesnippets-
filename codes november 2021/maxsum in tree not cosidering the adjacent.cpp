648a71e9b1774a0506ee490951790431c3d83b53
Shubham <95shubham1@gmail.com>
2022-01-20 22:45:48 +0530

    max sum non adjacent nodes

--- codes november 2021/maxsum in tree not cosidering the adjacent.cpp
+++ codes november 2021/maxsum in tree not cosidering the adjacent.cpp
@@ -51,3 +51,30 @@ if(root->right){
 
 return sum;	
 }
+
+
+int main()
+{
+	
+	struct node* root=new node(1;
+	
+	root->left=new node(2);
+	
+	root->right=new node(3);
+	
+	
+	root->left->left=new node(1);
+	
+	root->right->left=new node(4);
+	
+	
+	root->right->right=new node(5);
+	
+	map<struct node*,int>mp;
+	int ans=maxsum(root,mp);
+	
+	cout<<ans;
+	
+	)
+	
+}
