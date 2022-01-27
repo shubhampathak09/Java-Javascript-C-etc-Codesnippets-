dbafcb88788f31d374bde9efa4584550f6e4d20b
Shubham <95shubham1@gmail.com>
2022-01-27 20:40:35 +0530

    attemp towards solving interval dp

---
+++ codes november 2021/strange printer.cpp
@@ -0,0 +1,78 @@
+// STRANGE printer
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int turn(string s,int i,int j){
+	
+	if(i>j)
+	return 0;
+	
+	int turns=turn(s,i,j-1)+1;    // return min step to print i to j
+	
+	for(int k=i;k<j;k++){
+		if(s[j-1]==s[k-1])
+		turns=min(turn(s,i,k)+turn(s,k+1,j-1),turns);
+		
+	}
+	
+	return turns;
+}
+
+int main()
+{
+	
+	
+	cout<<turn("abcccaa",0,7)-1;
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+2020
+associate qa enginner
+
+api testing
+-intellij, ui codechef, ghost 
+-mani8al teting post mant api , ide coedbcfjf
+-fcbcjcbc c jc
+-e commerce marqude queue
+- 35 frbdvf
+
+-----------------------------------5 chcdncnccbcjcbgddc
+dcgdghcsdgxxasxvx
+cdhcc
+
+
+
+// manual testing
+ // wat to move into automated testing framework
+ .//
+ //ch
+ */
