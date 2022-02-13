66944a1ad88d01ea8b1ac04b8801a3818c2f059b
Shubham <95shubham1@gmail.com>
2022-02-13 21:34:37 +0530

    string basivs

---
+++ codes november 2021/string basics 1.cpp
@@ -0,0 +1,20 @@
+// strings basics
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+	
+	
+	// char s[1000]={'1','a','b'};
+	
+	string s;  //Dynamic Array
+	
+	s="hello world";
+	
+	cout<<s<<endl;
+	
+	
+	
+}
