315af485a81e9e0bb3b16ef9538b3d6c72943958
Shubham <95shubham1@gmail.com>
2022-02-13 21:52:31 +0530

    more on stl string find method

---
+++ codes november 2021/string stl find.cpp
@@ -0,0 +1,39 @@
+// find
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+string par="cd kcec fcfc kc kc rfr4 fcwx edeegv kc ddedce kcf fcced kdccecec vc4rw kcfcff ffeffceedeekcfi vwddcene  eeld kc cefcce";
+
+string word;
+
+getline(cin,word);
+
+// find all occurences of string
+
+
+int index=par.find(word);
+
+
+
+
+/*
+if(index !=-1){
+	cout<<"first occurence is.."<<index<<endl;
+}
+
+index =par.find(word,index+1);
+
+
+if(index!=-1){
+	cout<<"second occurence is.."<<index<<endl;
+}
+*/
+
+
+}
+
+ 
