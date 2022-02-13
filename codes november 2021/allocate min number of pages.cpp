26221fef540fe6d30f9d4a70ac478c3e06a6a877
Shubham <95shubham1@gmail.com>
2022-02-13 10:49:17 +0530

    added some problem sets

--- codes november 2021/allocate min number of pages.cpp
+++ codes november 2021/allocate min number of pages.cpp
@@ -85,3 +85,24 @@ int main(){
 	
 	cout<<allocatePages(a,n,k);
 }
+
+
+
+// Related problem
+/*
+Related Problems For Practice :
+Book Allocation Problem (GFG)
+Aggressive cow (spoj)
+Prata and roti (spoj)
+EKO (spoj)
+Google kickstart A Q-3 202
+Painter Partition Problem
+1482 Minimum Number of Days to Make m Bouquets
+1283 Find the Smallest Divisor Given a Threshold
+1231 Divide Chocolate
+1011 Capacity To Ship Packages In N Days
+875 Koko Eating Bananas
+Minimize 
+774 Max Distance to Gas Station
+410 Split Array Largest Sum
+*/
