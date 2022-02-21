8e656a63328df4ef31b1e8ee1b642a7f288254dd
Shubham <95shubham1@gmail.com>
2022-02-21 09:45:50 +0530

    Binary tree node class

---
+++ JavaCodeWars/src/Trees/BinaryTreeNode.java
@@ -0,0 +1,18 @@
+package Trees;
+
+public class BinaryTreeNode {
+
+
+    class Node{
+        
+        int data;
+        Node left,right;
+
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+
+}
