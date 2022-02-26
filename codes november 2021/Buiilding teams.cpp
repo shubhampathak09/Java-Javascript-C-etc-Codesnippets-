1e11570e8dafb0bac6691bedc6fa18f51b06fee3
Shubham <95shubham1@gmail.com>
2022-02-26 12:07:14 +0530

    building teams

--- codes november 2021/Buiilding teams.cpp
+++ codes november 2021/Buiilding teams.cpp
@@ -3,4 +3,23 @@
 // 2 color prroblem
 
 
+//  critical when we have some contracdiction
 
+// if we encounter some contractdiction then impossible to colr
+
+
+// impl
+
+#include<bits/stdc++.h>
+using namespace  std;
+
+
+int n,m;
+
+
+vector<vector<int>>g;
+
+vector<int>colr;
+
+
+vector<bool>vis;
