6888f9b97aa7875c506064d4214338122c7a6df7
Shubham <95shubham1@gmail.com>
2022-02-28 10:13:00 +0530

    reverse ll

---
+++ JavaCodeWars/src/LeetCodeDaily/ReverseLinkedList.java
@@ -0,0 +1,42 @@
+package LeetCodeDaily;
+
+public class ReverseLinkedList {
+
+
+    public static class ListNode
+    {
+
+        int data;
+        ListNode next;
+        ListNode(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+
+
+    public static void main(){
+
+        ListNode head=new ListNode(3);
+
+        head.next=new ListNode(5);
+
+        head.next.next=new ListNode(9);
+
+        ListNode prev=null;
+
+        while(head!=null){
+
+            ListNode next=head.next;
+
+            head.next=prev;
+
+            prev=head;
+
+            head=next;
+        }
+
+        
+    }
+
+}
