39e689f02e405c9e324cc68a74c9585bfeaf1d27
Shubham <95shubham1@gmail.com>
2022-02-28 08:18:37 +0530

    pascals triagke

---
+++ JavaCodeWars/src/LeetCodeDaily/PascalsTriangle.java
@@ -0,0 +1,29 @@
+package LeetCodeDaily;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangle {
+
+
+    public static List<List<Integer>> generate(int numrows){
+
+        List<List<Integer>> triangle=new ArrayList<>();
+
+        if(numrows==0)
+            return  triangle;
+
+
+        List<Integer> firs_row=new ArrayList<>();
+          
+
+        return null;
+    }
+
+    public static void main(String[] args){
+
+
+    }
+
+
+}
