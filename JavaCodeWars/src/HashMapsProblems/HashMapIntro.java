ad673048b3d047d5c264737593cc5958a4bb3812
Shubham <95shubham1@gmail.com>
2022-03-04 19:42:48 +0530

    codes

---
+++ JavaCodeWars/src/HashMapsProblems/HashMapIntro.java
@@ -0,0 +1,21 @@
+package HashMapsProblems;
+
+import java.util.*;
+
+public class HashMapIntro {
+
+    public static void main(String[] args) {
+
+
+    HashMap<String, Integer> hm = new HashMap<>();
+
+    hm.put("India",120);
+    hm.put("China",200);
+
+
+
+
+}
+
+
+}
