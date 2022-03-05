7ea620ba4ecd38212aaef61df18e35d63a8033b2
Shubham <95shubham1@gmail.com>
2022-03-05 08:34:32 +0530

    java todo

--- JavaCodeWars/src/ImmutableList/Copof.java
+++ JavaCodeWars/src/ImmutableList/Copof.java
@@ -58,4 +58,34 @@ public class Copof {
     Endgame Databases
     Thoughts
 */
+
+// paradigms of java:-
+
+/*
+-is java pure object oriented orogramming language
+-what is difference between .equals() and ==
+-details of public static void main
+-static public void main instead and use jusr public void main will the code execute
+-why is the main method in java static
+-what happends when main method is not delcared as static
+-what if the main method is declared as private
+-what if i wtire string a[] instead of string args[] or with var args
+-can we overload /override static methods oin java ? what happens when we try to override the static method
+-diffecen between stringbuffer and stringbuilder
+-what should be prefered when theere are lot of updates to be done in data ? string or string buffer
+-why strings are immutable in java
+-are strings thread safe
+-hwo namy ibjects will be created for follwoing code String str="apple" String st3=new String("apple");
+-what is grabage collection in java
+-find all elgible objects for  garbage colection from java code
+-algorithm behind jqava colleection? how is it done
+
+
+
+
+ */
+
+
+
 }
+
