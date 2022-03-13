b19ff9d2a8648d4b150573dad420902eb88630ca
Shubham <95shubham1@gmail.com>
2022-03-14 00:00:00 +0530

    life universe and everything

---
+++ codes november 2021/spoj_life_universe_everything.cpp
@@ -0,0 +1,34 @@
+// spoj classical life universe and everything
+
+/*
+TEST - Life, the Universe, and Everything
+#basic #tutorial #ad-hoc-1
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	
+
+int t;
+
+while(cin>>t){
+	
+	
+	if(t!=42){
+		cout<<t<<endl;
+	}else
+	break;
+	
+}	
+	
+/*
+ps: do read the following
+	Douglas Adams, The Hitchhiker's Guide to the Galaxy
+	
+	*/
+	
+		
+}
