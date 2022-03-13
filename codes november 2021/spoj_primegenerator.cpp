310c74607072a519c2954d94f404ce9452b7669d
Shubham <95shubham1@gmail.com>
2022-03-14 00:18:27 +0530

    prime genrator using sieve

--- codes november 2021/spoj_primegenerator.cpp
+++ codes november 2021/spoj_primegenerator.cpp
@@ -3,22 +3,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+vector<int>prime(100001);
 
 void sieve(){
 	
-	vector<int>prime(100001);
+	
 	
 	prime[0]=prime[1]=1;
 	
 	
-	for(int i=2;i*i<prime.size;i++){
+	for(int i=2;i*i<prime.size();i++){
 		
 		
 		if(prime[i]==0){
 			
-			for(int j=i;j<prime.size();j+=i){
+			for(int j=i*i;j<prime.size();j+=i){
 				
-				prime[j=1;
+				prime[j]=1;
 				
 			}
 		}
@@ -30,6 +31,52 @@ void sieve(){
 int main()
 {
 	
+	// fo bigger input size make sure to use lonng long
+	
+	sieve();
+	
+/*	
+	for(int i=0; i< 100;i++){
+		cout<<i<<" "<<prime[i]<<" "<<endl;
+	}
+*/	
+	int t;
+	
+	cin>>t;
+	
+	while(t--){
+	
+	int m,n;
+	
+	cin>>m>>n;
 	
 	
+	for(int i=m;i<=n;i++){
+	
+//	cout<<"dcd";
+		
+		if(prime[i]==0){
+			
+            cout<<i<<endl;			
+			
+		}
+		
+	}
+	
+	}
 }
+
+// input and output 
+
+/*
+2
+1 10
+2
+3
+5
+7
+3 5
+3
+5
+*/
+
