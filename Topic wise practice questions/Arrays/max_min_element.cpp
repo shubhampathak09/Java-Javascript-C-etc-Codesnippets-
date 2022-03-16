a9a7174a082ae5123ff7369978d7c2a4aacaa189
Shubham <95shubham1@gmail.com>
2022-03-16 21:16:30 +0530

    min and max element in array

---
+++ Topic wise practice questions/Arrays/max_min_element.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a[]={100,11,445,1,330,3000};
+	
+	int n=sizeof(a)/sizeof(a[0]);
+	
+	
+	int mx=INT_MIN;
+	int mn=INT_MAX;
+	
+	for(int i=0;i<n;i++){
+		
+		
+		if(a[i]>mx)
+		mx=a[i];
+		
+		if(a[i]<mn)
+		mn=a[i];
+		
+	}
+	
+	
+	cout<<"Max element in array is.."<<mx<<endl;
+	
+	cout<<"Min element in array is.."<<mn;
+}
+
