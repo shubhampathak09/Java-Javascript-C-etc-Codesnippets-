f8669fd4172fa332a3263047b59e2ae737cf5428
Shubham <95shubham1@gmail.com>
2022-03-17 12:58:44 +0530

    fixing the jump game for corner cases

--- JavaCodeWars/src/LeetCodeDaily/JumpGame2.java
+++ JavaCodeWars/src/LeetCodeDaily/JumpGame2.java
@@ -5,7 +5,7 @@ public class JumpGame2 {
     public static void main(String[] args){
 
 
-        int[] nums={2,3,1,1,4};
+        int[] nums= {5,9,3,2,1,0,2,3,3,1,0,0};
 
          int[] dp=new int[nums.length];
 
@@ -14,22 +14,33 @@ public class JumpGame2 {
 
          for(int i= nums.length-2;i>=0;i--){
 
-             if(nums[i]>0){
 
+             if(nums[i]==0){
+
+                 dp[i]=Integer.MAX_VALUE;
+                 continue;
+
+             }
+
+             if(nums[i]>0){
+               //  System.out.println(i);
                  int min=Integer.MAX_VALUE;
 
              for(int j=1;j<=nums[i]&&i+j< nums.length;j++) {
+               //  System.out.println(i);
+               //  System.out.println(i+j);
+            //     System.out.println(dp[i+j]);
 
-               if(dp[i+j]!=0) {
                    min = Math.min(dp[i + j], min);
-                   System.out.println(min);
-               }
+             //      System.out.println(min);
+
              }
 
              if(min!=Integer.MAX_VALUE)
              dp[i]=1+min;
              //else
-
+            else
+                dp[i]=Integer.MAX_VALUE;
              }
 
 
@@ -38,9 +49,9 @@ public class JumpGame2 {
 
 
          for(int i=0;i<dp.length;i++){
-          //   System.out.println(dp[i]);
+             System.out.print(dp[i]+" ");
          }
-
+System.out.println(" ");
      System.out.println(dp[0]);
 
 
