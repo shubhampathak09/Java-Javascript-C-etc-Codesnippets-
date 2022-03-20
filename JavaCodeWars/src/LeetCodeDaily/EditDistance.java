4e109a6b506d7a45c539314b86fa820bce4a8011
Shubham <95shubham1@gmail.com>
2022-03-20 10:49:50 +0530

    edit distance

---
+++ JavaCodeWars/src/LeetCodeDaily/EditDistance.java
@@ -0,0 +1,44 @@
+package LeetCodeDaily;
+
+public class EditDistance {
+
+
+
+    public static int Solution(String a,String b,int n,int m){
+
+        if(n==0){
+            return m;
+        }
+        if(m==0)
+            return n;
+
+        if(a.charAt(n-1)==b.charAt(m-1))
+            return Solution(a,b,n-1,m-1);
+
+        else{
+
+
+            // do insert remove and intercahnge
+
+            return 1 + Math.min(Solution(a,b,n-1,m),Math.min(Solution(a,b,n,m-1),Solution(a,b,n-1,m-1)));
+        }
+
+
+    }
+
+    public  static void main(String [] args){
+
+
+        String word1="horse";
+
+        String word2="ros";
+
+        int ans= Solution(word1,word2,word1.length(),word2.length());
+
+        System.out.println(ans);
+
+        return;
+
+    }
+
+}
