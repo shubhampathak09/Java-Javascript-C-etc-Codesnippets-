cecd9312456ad7fc4a6ad76fdc2796ec19dc53e8
Shubham <95shubham1@gmail.com>
2022-03-21 18:08:32 +0530

    max area rectangle

--- JavaCodeWars/src/LeetCodeDaily/MaxAreaRectangle.java
+++ JavaCodeWars/src/LeetCodeDaily/MaxAreaRectangle.java
@@ -1,5 +1,7 @@
 package LeetCodeDaily;
 
+import java.util.Stack;
+
 public class MaxAreaRectangle {
 /*
     Given a rows x cols binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
@@ -9,7 +11,80 @@ public class MaxAreaRectangle {
     public static int findMaxRectangle(int []a){
 
 
-        return 0;
+
+        int n=a.length;
+        int[]left=new int[n];
+        int[]right=new int[n];
+
+        int mx=Integer.MIN_VALUE;
+
+        left[0]=-1;
+
+        Stack<Integer>s=new Stack<>();
+        s.push(0);
+        for(int i=1;i<n;i++){
+
+            while(!s.empty()&&a[i]<=a[s.peek()]){
+                s.pop();
+
+
+            }
+            if(s.empty()){
+                left[i]=-1;
+
+            }else
+            {
+                left[i]=s.peek();
+
+            }
+            s.push(i);
+        }
+
+
+        s.clear();
+
+        right[n-1]=n;
+        s.push(n-1);
+
+        for(int i=n-2;i>=0;i--){
+
+            while(!s.empty()&&a[i]<=a[s.peek()]){
+                s.pop();
+            }
+            if(s.empty()){
+                right[i]=n;
+            }else
+            {
+                right[i]=s.peek();
+            }
+            s.push(i);
+        }
+
+
+        for(int i=0;i<n;i++){
+
+            mx=Math.max((right[i]-left[i]-1)*a[i],mx);
+
+        }
+
+        for(int i=0;i<a.length;i++)
+            System.out.print(a[i]+" ");
+            System.out.print(mx);
+
+
+
+        System.out.println();
+
+        for(int i=0;i<n;i++)
+            System.out.print(left[i]+" ");
+        System.out.println();
+
+        for(int i=0;i<n;i++)
+            System.out.print(right[i]+" ");
+
+        System.out.println();
+
+         return mx;
 
     }
 
@@ -50,7 +125,7 @@ public class MaxAreaRectangle {
 
      int mx=0;
       for(int i=0;i<n;i++){
-          
+
           mx=Math.max(mx,findMaxRectangle(mat[i]));
 
       }
@@ -84,6 +159,7 @@ public class MaxAreaRectangle {
 
         int ans=maximalRectangle(c);
 
+        System.out.println(ans);
     }
 
 }
