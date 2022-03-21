8cbde8656f7735f74048d6f40202bc02822db87d
Shubham <95shubham1@gmail.com>
2022-03-21 17:30:25 +0530

    largest rect hist some fixes

--- JavaCodeWars/src/LeetCodeDaily/MaxAreaRectangle.java
+++ JavaCodeWars/src/LeetCodeDaily/MaxAreaRectangle.java
@@ -1,11 +1,17 @@
 package LeetCodeDaily;
 
-import java.util.ArrayList;
-
 public class MaxAreaRectangle {
 /*
     Given a rows x cols binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
 */
+
+
+    public static int findMaxRectangle(int []a){
+
+
+        return 0;
+
+    }
 
     public static int maximalRectangle(char[][] matrix) {
 
@@ -15,43 +21,61 @@ public class MaxAreaRectangle {
         int n=matrix.length;
         int m=matrix[0].length;
 
-        ArrayList<ArrayList<Integer>>mat=new ArrayList<>(n);
 
-        for(int i=0;i<m;i++){
-            ArrayList<Integer>ar=new ArrayList<>();
-            mat.add(ar);
-        }
+         int[][] mat=new int[n][m];
+
+
 
         for(int i=0;i< n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j]=='1')
-                    mat.get(i).add(1);
+                    mat[i][j]=1;
                 else
-                    mat.get(i).add(0);
+                    mat[i][j]=0;
             }
         }
 
-      for(int i=0;i<mat.size();i++){
-          for(int j=0;j<mat.get(i).size();j++){
+      for(int i=1;i<n;i++){
+          for(int j=0;j<m;j++){
 
-              if(i==0)
-              continue;
-
-              if(mat.get(i).get(j)==0)
-              mat.get(i).add(0);
+              if(mat[i][j]==0)
+              mat[i][j]=0;
               else
-              mat.get(i).add(1+mat.get(i-1).get(j));
+              mat[i][j]+=mat[i-1][j];
           }
 
+
+
       }
 
+     int mx=0;
+      for(int i=0;i<n;i++){
+          
+          mx=Math.max(mx,findMaxRectangle(mat[i]));
 
-      // fuck
-      for(ArrayList<Integer>a:mat){
+      }
+
+      // fuck  --atemot 1 with arraylist
+
+        //fuck   attempt 2 with arraylist
+
+        //seitched to simple 2 d arrays
+  /*
+      for(ArrayList<Integer>a:mat2){
           System.out.println(a);
       }
+*/
+/*
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.print(mat[i][j]+" ");
+            }
+            System.out.println();
+        }
+  */
+       // System.out.println(mat[1]);
 
-      return 0;
+      return mx;
     }
 
     public static void  main(String[] args){
