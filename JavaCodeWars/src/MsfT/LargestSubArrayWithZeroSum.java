ab5acf5375b192da63bdff7be6a8728f999a8ada
Shubham <95shubham1@gmail.com>
2022-03-28 18:17:52 +0530

    max subarray length with sum zero

--- JavaCodeWars/src/MsfT/LargestSubArrayWithZeroSum.java
+++ JavaCodeWars/src/MsfT/LargestSubArrayWithZeroSum.java
@@ -1,4 +1,47 @@
 package MsfT;
 
+import java.util.HashMap;
+
 public class LargestSubArrayWithZeroSum {
+
+    // 60 to go
+
+    public static void main(String[] args){
+
+
+        int[] arr={15,-2,2,-8,1,7,10,23};
+
+        int i=-1;
+
+        int sum=0;
+
+        HashMap<Integer,Integer>map=new HashMap<>();
+
+
+        map.put(sum,-1);
+
+       int maxlen=0;
+
+        while(i<arr.length-1){
+
+            i++;
+            sum+=arr[i];
+
+            if(map.containsKey(sum)==false){
+                map.put(sum,i);
+            }else
+            {
+                int length= (i-map.get(sum));
+                if(length>maxlen){
+                    maxlen=length;
+                }
+            }
+
+        }
+
+        System.out.println(maxlen);
+
+
+    }
+
 }
