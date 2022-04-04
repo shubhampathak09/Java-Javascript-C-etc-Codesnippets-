8c0b536aa3d11427adf160b11aff1d5aba32cee7
Shubham <95shubham1@gmail.com>
2022-04-04 23:59:19 +0530

    le

---
+++ Microsoft preparation/zigzagr.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+     
+        
+        vector<vector<int>>result;
+        
+        if(root==NULL)return result;
+        
+        
+        queue<TreeNode*>q;
+        
+        
+        q.push(root);
+        
+        
+        int l=0;
+        
+        while(!q.empty()){
+            
+            l++;
+            
+            int n=q.size();
+            vector<int>curr;
+            for(int i=1;i<=n;i++){
+                
+                
+                auto u=q.front();
+                curr.push_back(u->val);
+                if(u->left){
+                    q.push(u->left);
+                }
+                if(u->right){
+                    q.push(u->right);
+                }
+                    
+            }
+            if(l%2==0)
+            {
+                reverse(curr.begin(),curr.end());
+                result.push_back(curr);
+            }   
+                
+            else
+            result.push_back(curr);
+            
+        }
+        
+        
+        return result;
+    }
+};
