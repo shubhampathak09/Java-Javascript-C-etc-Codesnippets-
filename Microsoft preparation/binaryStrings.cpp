8ccb7ef9b83f785ee17bbcecf4e03e552d7c05aa
Shubham <95shubham1@gmail.com>
2022-04-17 20:37:33 +0530

    just some practise

---
+++ Microsoft preparation/binaryStrings.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+
+int countString(int n){
+	
+	
+ int a=1;
+ int b=1;
+ 
+ for(int i=2;i<=n;i++){
+ 	
+ 	int na=b;
+ 	int nb=a+b;
+ 	
+ 	a=na;
+ 	b=nb;
+ 	
+ }
+
+return a+b;	
+	
+}
+
+int main(){
+	
+// test here
+	
+	
+}
---
+++ Microsoft preparation/string processing-1.cpp
@@ -0,0 +1,13 @@
+//
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	
+	char s[100]="1 -> 2 -> 3 -> 4 -> null";
+	
+	string ans=strtok(s," ");
+	
+	cout<<ans;
+	 
+}
