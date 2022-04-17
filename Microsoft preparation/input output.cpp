a86fb29f580f0a51ffd5e1dd6af307a893fce37f
Shubham <95shubham1@gmail.com>
2022-04-17 09:31:27 +0530

    precision

--- Microsoft preparation/input output.cpp
+++ Microsoft preparation/input output.cpp
@@ -1,11 +1,24 @@
-#include <iostream> // or <cstdio>
-using namespace std;
-char digits[100]; // using global variables in contests can be a good strategy
-int N;
-int main() {
-scanf("%d", &N);
-while (N--) { // we simply loop from N, N-1, N-2, ... 0
-scanf("0.%[0-9]...", &digits); // surprised?
-printf("the digits are 0.%s\n", digits);
-} }
+//#include <iostream> // or <cstdio>
+//using namespace std;
+//char digits[100]; // using global variables in contests can be a good strategy
+//int N;
+//int main() {
+//scanf("%d", &N);
+//while (N--) { // we simply loop from N, N-1, N-2, ... 0
+//scanf("0.%[0-9]...", &digits); // surprised?
+//printf("the digits are 0.%s\n", digits);
+//} }
 
+
+// test
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	
+	double d=5.489581230;
+	
+	printf("%0.2f",d);
+	
+}
