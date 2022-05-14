d49a04aa24a2d560c6fe91eb26b31e493ef9f608
Shubham <95shubham1@gmail.com>
2022-05-14 18:57:59 +0530

    revision graphs

--- Microsoft preparation/revision.cpp
+++ Microsoft preparation/revision.cpp
@@ -35,6 +35,54 @@ int connected_comp(){
 }
 
 
+// sp using df  not appicanle
+
+
+
+void bs(int u){
+	
+	
+	queue<int>bfs_q;
+	
+	bfs_q.push_back(u);
+	
+	
+	dist[u]=0;
+	vis[u]=true;
+	
+	while(!bfs_q.empty()){
+		
+		u=bfs_q.front(u);
+		bfs_q.pop();
+		
+		
+		for(auto v:g[u]){
+			
+			
+			if(!vis[v]){
+				
+				
+				bfs_q.push(v);
+				vis[v]=true;
+				dist[v]=dist[u]+1;
+				
+				
+			}
+			
+		}
+		
+	}
+	
+	
+	
+	
+}
+
+
+
+
+
+
 int main(){
 	
 	
