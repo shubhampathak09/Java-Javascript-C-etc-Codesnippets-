c6033d05372d9961f17a952823216b233166013b
Shubham <95shubham1@gmail.com>
2022-05-14 09:27:40 +0530

    Raju and his Trip

---
+++ Microsoft preparation/Raju and His Trip.cpp
@@ -0,0 +1,23 @@
+//Rahu and his trip
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+	
+	
+	int n;
+	
+	cin>>n;
+	
+	
+	if(n%5==0|| n%6==0){
+		cout<<"YES"<<endl;
+	}
+	else
+	{
+		cout<<"NO";
+	}
+	
+}
