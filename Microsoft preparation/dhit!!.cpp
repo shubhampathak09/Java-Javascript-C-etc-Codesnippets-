f249beba8568e55bfe151853f389c4f7b59240c5
Shubham <95shubham1@gmail.com>
2022-05-22 20:33:21 +0530

    finally some woking logic for revese string

---
+++ Microsoft preparation/dhit!!.cpp
@@ -0,0 +1,36 @@
+// repeated dna sequqences
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	
+	
+	string s="AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+	
+    int i=0;
+    int j=0;
+    map<string,int>hm;
+    while(j<s.legth()){
+    	
+    	if(j-i+1<10){
+    		j++;
+		}
+    	
+    	if(j-i+1==10){
+    		string curr=s.substr(i,j-i+1);
+    		m[cur]++;
+		}
+    	else{
+    		i++;
+    		if(j-i+1==10)
+    		{
+    			string curr=s.substr()
+			}
+		}
+	}\\
+	// fuck see unnale to do sliding windoww
+	
+	
+	
+}
