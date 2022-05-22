dcb723bdd91dc6a783cb4ef3b1c391f6ae305ed1
Shubham <95shubham1@gmail.com>
2022-05-22 23:37:00 +0530

    generate all possible subssets

---
+++ Microsoft preparation/repeated dna sequence.cpp
@@ -0,0 +1,50 @@
+// repeated dna sequence
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int countset(int n){
+	
+	int ct=0;
+	while(n>0){
+		if(n & 1){
+			ct++;
+		}
+		n=n>>1;
+	}
+
+     return ct;
+	 	
+}
+
+int main(){
+	
+	 string s="AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+	
+	int n=s.length();
+	
+	
+	int mask=1<<n;
+	
+	map<string,int>mp;
+	
+	for(int i=0;i<mask;i++){
+		
+		int crmask=i;
+	    if(countset(crmask)==10)
+		  {
+		  	cout<<crmask;
+			string crr=""; 	
+		for(int j=0;j<s.length();j++){
+			if(crmask & (1<<j)){
+				crr+=s[j];			}
+		}
+		mp[crr]++;
+	}
+	}
+	
+	  //cout<<mp.size();
+	///  cout<<countset(7);
+	  
+}
