81b2c0d73ed3bdc0eab38c6e02e49345b90e59ef
Shubham <95shubham1@gmail.com>
2022-06-02 23:31:03 +0530

    codes

---
+++ JavaCodeWars/src/GS/DuplicatesBrackets.java
@@ -0,0 +1,24 @@
+package GS;
+
+import java.util.Stack;
+
+public class DuplicatesBrackets {
+
+    public static void main(String[] args){
+
+
+        // ((a+b)) +(c+d)) ->false
+
+        // if extra brackets detected true else give false
+
+
+     Stack<Character> st=new Stack<>();
+
+     String s=""
+
+
+
+
+    }
+
+}
