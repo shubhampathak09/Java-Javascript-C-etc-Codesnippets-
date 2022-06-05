4086cd2b264f2a92a0e1e5fa7c8bfb913cd1e9ab
Shubham <95shubham1@gmail.com>
2022-06-05 12:35:43 +0530

    single patterb

---
+++ Microsoft preparation/nvidia to do.cpp
@@ -0,0 +1,24 @@
+// consrtucotr 
+// kth largest elemt bst when  modofication is not allow
+// reverse linked list in k and group of 2
+// keys and rooms
+// puzzle questions
+//time complexistu
+//search elememt i soretd arrar peiovited
+// rorated arrrau search
+//min in roated aaray
+//design stack upportsget min
+// insert update delee
+ // lry cache
+ // hahs map internal impl
+ // structur paading and oofset
+ //multi thread one thread givinng even numbers
+// and other thread odd numebr
+ // tow thrad trying to read same resoiruce
+ //cncurent modifactin exception
+ //skip locks 
+ //deadlock and avoiddance
+ // sprin dead locks
+ // cyclic depecies
+ //autowiring
+ //
