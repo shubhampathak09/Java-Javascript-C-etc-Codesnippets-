d0bf39036f72d2742543a45e661a514e6c6352c8
Shubham <95shubham1@gmail.com>
2022-06-06 21:29:58 +0530

    stream api

---
+++ JavaCodeWars/src/stream/StreamEx1.java
@@ -0,0 +1,23 @@
+package stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamEx1 {
+
+
+    public static void main(String[] args){
+
+        List<Integer> arr= Arrays.asList(2,3,19,0,10,14,78,91);
+
+        List<Integer> arr1=arr.stream()
+           .filter(x -> x % 2==0)
+           .collect(Collectors.toList());
+
+
+        System.out.println(arr1);
+
+    }
+
+}
