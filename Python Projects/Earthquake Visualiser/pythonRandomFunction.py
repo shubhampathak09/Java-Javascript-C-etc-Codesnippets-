8a08c5117eecb409f8ff3a3b8ad364f5b38e6563
Shubham <95shubham1@gmail.com>
2023-01-01 18:37:27 +0530

    genetic algo generate python

--- Python Projects/Earthquake Visualiser/pythonRandomFunction.py
+++ Python Projects/Earthquake Visualiser/pythonRandomFunction.py
@@ -6,6 +6,22 @@ Created on Fri Dec 30 19:38:57 2022
 """
 import random
 
-for i in range(100):
-    a=(random.randint(0, 1))
-    print(a)
+def generate(len):
+ s=""   
+ for i in range(len):
+    s+=str((random.randint(0, 1)))
+    
+ return s   
+
+
+def mutate(len,p):
+    return
+
+def printText():
+    r=random.random()
+    if(r<0.8):
+        print('dogs')
+    elif r <0.9:
+        print('cats')
+    else:
+        print('bats')    
